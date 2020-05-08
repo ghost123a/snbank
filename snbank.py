7b2f26c9d24e9d1bba12333f8b4e5b643f21915d
ghosted123a <61979494+ghost123a@users.noreply.github.com>
2020-05-08 21:19:43 +0100

    Add files via upload

--- snbank.py
+++ snbank.py
@@ -6,11 +6,11 @@ import os
 print('Welcome to SN BANK')
 print('*' * 18)
 
-print('Please choose an option''\n 1 Staff Login''\n 2 Close App')
 
+print('Please choose an option''\n 1 Staff Login''\n 2 Close App')
 choice = int(input('>: '))
 
-if choice == 1:
+while choice == 1:
     username = str(input('username: '))
     password = str(input('password: '))
 
@@ -39,40 +39,44 @@ if choice == 1:
             choice = int(input('>: '))
     login_check()
 
-session = open('session.txt', 'w')
+    while True:
+        session = open('session.txt', 'w')
 
-print('Please chose an option''\n1. Create new bank account''\n2. Check Account Details''\n3. Logout')
-option = int(input('>: '))
-if option == 1:
-    account_name = str(input('Account Name: ')).lower()
-    opening_balance = float(input('Opening Balance: '))
-    account_type = str(input('Account Type: ')).lower()
-    account_email = input('Account email: ')
-    account_number = ''.join(map(str, [random.randint(1,9) for a in range(0,10)]))
-    customer_details = open('customer.txt', 'w+')
-    customer_details.write('Account Name: %s\n' % account_name)
-    customer_details.write('Opening Balance: %s\n' % opening_balance)
-    customer_details.write('Account Type: %s\n' % account_type)
-    customer_details.write('Account email: %s\n' % account_email)
-    customer_details.write('Account Number: %s\n' % account_number)
-    customer_details.close()
+        print('Please chose an option''\n1. Create new bank account''\n2. Check Account Details''\n3. Logout')
+        option = int(input('>: '))
+        if option == 1:
+            account_name = str(input('Account Name: ')).lower()
+            opening_balance = float(input('Opening Balance: '))
+            account_type = str(input('Account Type: ')).lower()
+            account_email = input('Account email: ')
+            account_number = ''.join(map(str, [random.randint(1,9) for a in range(0,10)]))
+            customer_details = open('customer.txt', 'w+')
+            customer_details.write('Account Name: %s\n' % account_name)
+            customer_details.write('Opening Balance: %s\n' % opening_balance)
+            customer_details.write('Account Type: %s\n' % account_type)
+            customer_details.write('Account email: %s\n' % account_email)
+            customer_details.write('Account Number: %s\n' % account_number)
+            customer_details.close()
 
-    print(f'Your Account Number is {account_number}')
-    print('Please chose an option''\n1. Create new bank account''\n2. Check Account Details''\n3. Logout')
-    option = int(input('>: '))
+            print(f'Your Account Number is {account_number}')
+            print('Please chose an option''\n1. Create new bank account''\n2. Check Account Details''\n3. Logout')
+            option = int(input('>: '))
 
-if option == 2:
-    input('input your Account Number: ')
-    account_details = open('customer.txt', 'r')
-    print(account_details.read())
+        if option == 2:
+            input('input your Account Number: ')
+            account_details = open('customer.txt', 'r')
+            print(account_details.read())
 
-    print('Please chose an option''\n1. Create new bank account''\n2. Check Account Details''\n3. Logout')
-    option = int(input('>: '))
+            print('Please chose an option''\n1. Create new bank account''\n2. Check Account Details''\n3. Logout')
+            option = int(input('>: '))
 
-if option == 3:
-    session.close()
-    os.remove("session.txt")
-    print('Please choose an option''\n 1 Staff Login''\n 2 Close App')
-    choice = int(input('>: '))
-else:
+        if option == 3:
+            session.close()
+            os.remove("session.txt")
+            print('Please choose an option''\n 1 Staff Login''\n 2 Close App')
+            choice = int(input('>: '))
+
+
+while choice == 2:
     print('Program Terminated''\nThank You and Good Bye')
+    break
